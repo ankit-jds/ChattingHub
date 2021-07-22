efbf03e51bcab795946f080daff7da87e5c6c7f6
ankitamboli <84713907+ankitamboli@users.noreply.github.com>
2021-07-23 00:17:11 +0530

    Update test.js
    Added the script for burger menu icon

--- test.js
+++ test.js
@@ -3,26 +3,31 @@ window.addEventListener('DOMContentLoaded',function () {
 
 });
 
-console.log(document.onload);
 var button=document.getElementsByClassName('send-button');
-console.log(button);
 
 button[0].addEventListener('click',function () {
 	var target=event.target;
-	console.log(target);
 	var element=document.getElementsByClassName('chat-display')[0];
 	var message=document.getElementById('type-area').value;
-	console.log(message);
-	var code=`<div class="message-row typer">
-				<div class="message typer">
-					<p class="message-text">${message}
-					</p>
-					<span class="timestamp">6:55am</span>
-					<img class="reply-button" src="icons/reply.png"></img>
-				</div>
-			</div>`;
-	element.innerHTML+=code;
-	document.getElementById('type-area').value="";
-	// console.log(text.innerHTML);
+	if (message!=""){
+		var code=`<div class="message-row typer">
+					<div class="message typer">
+						<p class="text">${message}
+						</p>
+						<span class="timestamp">6:55am</span>
+					</div>
+					<img class="share-button" src="icons/reply.png"></img>
+				</div>`;
+		element.innerHTML+=code;
+		document.getElementById('type-area').value="";
+	}
 },false);
-console.log(button);
+
+const menu=document.querySelector('.burger');
+const list=document.querySelector('.list');
+const chatWindow=document.querySelector('.chat-window');
+menu.addEventListener('click',()=>{
+	list.classList.toggle('show');
+	chatWindow.classList.toggle('hide');
+
+} )
